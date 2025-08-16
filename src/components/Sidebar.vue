1cc9b6c3f9cd91633c1c0b19853e778d84dc133d
jenkinchris11 <CJ204077@falmouth.ac.uk>
2025-08-16 23:26:17 +0100

    feat: integrate Vuetify for styling

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,42 +1,19 @@
 <script setup>
-import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <nav class="sidebar">
-    <h2 class="logo">My Site</h2>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-    <RouterLink to="/gallery">Gallery</RouterLink>
-    <RouterLink to="/social">Social</RouterLink>
-    <RouterLink to="/contact">Contact</RouterLink>
-    <RouterLink to="/projects">Projects</RouterLink>
-  </nav>
+  <v-navigation-drawer permanent>
+    <v-list>
+      <v-list-item title="My Site" class="text-h6"></v-list-item>
+      <v-list-item to="/" title="Home"></v-list-item>
+      <v-list-item to="/about" title="About"></v-list-item>
+      <v-list-item to="/gallery" title="Gallery"></v-list-item>
+      <v-list-item to="/social" title="Social"></v-list-item>
+      <v-list-item to="/contact" title="Contact"></v-list-item>
+      <v-list-item to="/projects" title="Projects"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <style scoped>
-.sidebar {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 200px;
-  height: 100vh;
-  background: #2c3e50;
-  padding: 1rem;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  color: #fff;
-}
-.sidebar a {
-  color: inherit;
-  text-decoration: none;
-}
-.sidebar a.router-link-active {
-  font-weight: bold;
-}
-.logo {
-  margin-bottom: 2rem;
-}
 </style>
