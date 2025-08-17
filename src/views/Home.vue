5ff6b0ce11f2f9e312dbf6e1bcff2cd6b0abb26c
jenkinchris11 <CJ204077@falmouth.ac.uk>
2025-08-17 01:26:59 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,12 +31,24 @@ const { imageUrl, tagline } = defineProps({
   align-items: center;
   background-size: cover;
   background-position: center;
+  font-family: "Playfair Display", serif;
+  font-optical-sizing: auto;
+  font-weight: <weight>;
+  font-style: normal;
 }
 .home {
   text-align: center;
+  font-family: "Playfair Display", serif;
+  font-optical-sizing: auto;
+  font-weight: <weight>;
+  font-style: normal;
 }
 .title {
   font-size: 3rem;
   margin-bottom: 1rem;
+  font-family: "Playfair Display", serif;
+  font-optical-sizing: auto;
+  font-weight: <weight>;
+  font-style: normal;
 }
 </style>
