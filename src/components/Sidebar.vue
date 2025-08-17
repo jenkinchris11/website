8691c37da221f553a2adfee4598b93f76990291c
jenkinchris11 <CJ204077@falmouth.ac.uk>
2025-08-17 01:35:44 +0100

    Update Sidebar.vue

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,7 +4,7 @@
 <template>
   <v-navigation-drawer permanent class="sidebar" elevation="0">
     <v-list>
-      <v-list-item title="Andrew Jenkin Sculpture" class="text-h6"></v-list-item>
+      <v-list-item title="Andrew Jenkin Sculpture" class="text-h3"></v-list-item>
       <v-list-item to="/" title="Home"></v-list-item>
       <v-list-item to="/about" title="About"></v-list-item>
       <v-list-item to="/gallery" title="Gallery"></v-list-item>
