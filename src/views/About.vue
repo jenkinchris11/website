915929c63395fbb8a1158f1b06abb204a931fe7a
jenkinchris11 <CJ204077@falmouth.ac.uk>
2025-08-18 19:55:34 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,12 +6,15 @@
         <h1>About Me</h1>
         <h2>Andrew Jenkin</h2>
         <p>I am an artist primarily occupied with physical sculptural work and have been constructing and fabricating for many years, due to my background in engineering. This has instilled me with many practical skills that I now apply in a more creative capacity.
+          <br>
           I choose to work in a wide range of mediums to maximise a diverse variety of works, and I have a vast knowledge of the best applications of materials for designs.
           <br><br>
           My recent work draws heavily on Geometry and Mathematics. Particularly Polygons, Polyhedra and sacred geometry, all of which have complex mathematical connotations.
+          <br>
           Having spent many years constructing objects from plans, patterns and templates, I have developed a passion of applying these processes to my own creative endeavours.
           <br><br>
           Over the last two years of my formal art training I have spent time with the much-acclaimed art sculptor Charles Hadcock, assisting him in his studio allowed me a privileged insight in the practice of a professional sculptor.
+          <br>
           Which has served to give me a desire to create larger scale works and a thirst to acquire commissions for public art sculpture.</p>
         </div>
       </div>
